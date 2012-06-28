e01c9f92247d4e8ccfeb59f5c7dcff3d0a724524
Mike Edwards <mikeedwards83@googlemail.com>
2012-06-28 20:32:04 +0100

    Updated SitecoreData.config so that an entire Sitecore solution can be converted to Mongo.
    Added support for the Workbox in Mongo.
    
    I have not made changes to support a complete Raven move or Workbox.

---
+++ src/SitecoreData.DataProviders/WorkflowHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SitecoreData.DataProviders
+{
+    public class WorkflowHistory
+    {
+        public Guid Id { get; set; }
+        public string Language { get; set; }
+        public int Version { get; set; }
+        public string OldState { get; set; }
+        public string NewState { get; set; }
+        public string Text { get; set; }
+        public string User { get; set; }
+        public DateTime Now { get; set; }
+            
+    }
+}
